9dcebb34895fb699ca42f81e7973307015ec74e5
marcarro <A00837236@tec.mx>
2022-11-17 14:07:46 -0600

    Agregar metodos de operacion interna

--- Playlist.h
+++ Playlist.h
@@ -1,11 +1,16 @@
-#include <Cancion.h>
-
 #include <string>
+#include <Cancion.h>
 
 class Playlist {
  private:
   std::string _nombre;
+  std::string _pedirDato(std::string dato);
+  int _numeroCancion = 0;
   Cancion _canciones[1000];
-
  public:
+  void agregarCancion();
+};
+
+void Playlist::agregarCancion() {
+  _canciones[_numeroCancion++];
 }
