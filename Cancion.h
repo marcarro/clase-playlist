6a2f380627e498ce21e6d37af16ac227bf1d409b
marcarro <A00837236@tec.mx>
2022-11-17 13:35:22 -0600

    Metodos set de cancion

--- Cancion.h
+++ Cancion.h
@@ -22,3 +22,19 @@ std::string Cancion::getNombre() { return _nombre; }
 std::string Cancion::getArtista() { return _artista; }
 std::string Cancion::getAlbum() { return _album; }
 std::string Cancion::getDuracion() { return _duracion }
+
+void Cancion::setNombre(std::string nombre) {
+  _nombre = nombre;
+}
+
+void Cancion::setArtista(std::string artista) {
+  _artista = artista;
+}
+
+void Cancion::setAlbum(std::string album) {
+  _album = album;
+}
+
+void Cancion::setDuracion(std::string duracion) {
+  _duracion = duracion;
+}
